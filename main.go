7f4b1da2dba5a782fcd203050a93471ccd79e57e
YuhengLinux <liuyuheng92@gmail.com>
2024-03-06 23:07:39 +0800

    add initial setup classes

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// retrieve environment variables and store into appConfig
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+	// create routers
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	// create v1 namespace router
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerError)
+
+	// mount subRouters, create & start server
+	router.Mount("/v1", v1Router)
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Creating server on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
